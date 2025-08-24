8fd033b998c072e0d7f088293a24ee2512e83239
Artem <rtmdlgj@gmail.com>
2025-08-24 11:46:37 +0300

    Second change

---
+++ Exem002/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Суммая: " + (first + second));
