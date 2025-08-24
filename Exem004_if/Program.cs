3418c169159411af247900755303cf38a114a8dd
Artem <rtmdlgj@gmail.com>
2025-08-24 12:00:41 +0300

    Change new fail

---
+++ Exem004_if/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите свое имя: ");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "артем")
+{
+    Console.WriteLine("Привееееет Артем :) ");
+} else
+{
+    Console.WriteLine("Пр, " + name);
+}
